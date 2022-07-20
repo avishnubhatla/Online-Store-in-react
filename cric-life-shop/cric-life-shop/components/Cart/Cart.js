abc37623bd790b9139632d2cbf764d31829fa408
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-20 11:05:15 -0400

    Update Cart.js

--- cric-life-shop/cric-life-shop/components/Cart/Cart.js
+++ cric-life-shop/cric-life-shop/components/Cart/Cart.js
@@ -1,1 +1,13 @@
-// Add Cart here
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export function Cart() {
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
