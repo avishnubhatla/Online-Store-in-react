2a18ae787783dadb07e44bd2f606d95123ecc242
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-20 11:06:35 -0400

    Update ProductDescription.js

--- cric-life-shop/cric-life-shop/components/ProductDescription/ProductDescription.js
+++ cric-life-shop/cric-life-shop/components/ProductDescription/ProductDescription.js
@@ -1,1 +1,13 @@
-# Add Product Description
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export function ProductDescription() {
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
