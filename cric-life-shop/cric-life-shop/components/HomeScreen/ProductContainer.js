305e93fb0ee7d629ca3b611ffb88016c15ec9166
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-07-22 10:35:29 -0400

    Update ProductContainer.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+
+export function ProductContainer() {
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
 
