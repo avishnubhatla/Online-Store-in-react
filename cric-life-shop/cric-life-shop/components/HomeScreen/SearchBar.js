4fa2a5c2f2a3cfff4b4eca8e9fb0b2e64ffd2147
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-22 10:06:55 -0400

    Update SearchBar.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
@@ -1,1 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export function SearchBar() {
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
+
 
