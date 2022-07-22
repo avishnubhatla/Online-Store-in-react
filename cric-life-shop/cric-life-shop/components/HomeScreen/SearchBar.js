d5e63e11e4587bf2016636d625b998bf0a4c4a17
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-22 10:54:42 -0400

    Update SearchBar.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
@@ -1,15 +1,30 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { TextInput, Text, View, StyleSheet } from 'react-native';
 
 export function SearchBar() {
   return (
-    <View>
+    <View style={styles.layout}>
+      <TextInput style={styles.searchBar} placeholder="Search CricLife Store"/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+  layout: {
+    flex: 1,
+    backgroundColor: "#7ae0ff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchBar: {
+    borderWidth: 1,
+    width: "80%",
+    height: "40%",
+    borderRadius: "10%",
+    backgroundColor: "white",
+    marginTop: 15,
+    padding: 5,
+  },
 });
 
 
