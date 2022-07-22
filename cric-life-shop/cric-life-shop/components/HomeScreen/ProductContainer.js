cb48b980ae0ee2d36075811c81a3672d11be938a
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-22 10:55:16 -0400

    Update ProductContainer.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Text, View, StyleSheet, Pressable } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export function ProductContainer() {
   return (
-    <View>
+    <View style={styles.layout}>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+  layout: {
+    flex: 5,
+  },
 });
+
+
 
