7fabddff4180076e7f814df71ad8b84bf1758573
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-07-22 10:35:06 -0400

    Update Product.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/Product.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/Product.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+
+export function Product() {
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
+
 
