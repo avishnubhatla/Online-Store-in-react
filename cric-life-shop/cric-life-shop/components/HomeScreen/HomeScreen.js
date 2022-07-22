47ad612a24705ae221fcf5d02a7edd55708e8ec9
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-07-22 10:34:39 -0400

    Update HomeScreen.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/HomeScreen.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/HomeScreen.js
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import {SearchBar } from "./SearchBar"
-
+import {Product} from './Product'
+import {ProductContainer} from './ProductContainer'
 export function HomeScreen() {
   return (
     <View>
