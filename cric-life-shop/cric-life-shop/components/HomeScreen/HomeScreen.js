2bc28762220c50f1eddb715d0bbd238e0f9791c0
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-22 10:08:08 -0400

    Update HomeScreen.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/HomeScreen.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/HomeScreen.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import {SearchBar } from "./SearchBar"
 
 export function HomeScreen() {
   return (
     <View>
+      <SearchBar />
     </View>
   )
 }
