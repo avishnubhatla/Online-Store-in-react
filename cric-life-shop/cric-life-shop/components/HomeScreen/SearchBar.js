6741cf8f9d14f468a25da36401f0a9b2d151ba36
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-24 11:03:26 -0400

    Update SearchBar.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
@@ -1,29 +1,29 @@
 import * as React from 'react';
 import { TextInput, Text, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export function SearchBar() {
   return (
-    <View style={styles.layout}>
+    <LinearGradient colors={['#FF0000', '#CC0033', "#990066", "#660099", "#3300CC", "#0000FF"]} style={styles.layout} start={{x: 0, y: 0.5}} end={{x: 1, y: 0.5}}>
       <TextInput style={styles.searchBar} placeholder="Search CricLife Store"/>
-    </View>
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    backgroundColor: "#7ae0ff",
     justifyContent: "center",
     alignItems: "center",
   },
   searchBar: {
-    borderWidth: 1,
+    borderWidth: 2,
     width: "80%",
-    height: 40,
+    height: "35%",
     borderRadius: "10%",
     backgroundColor: "white",
-    marginTop: 200,
     padding: 5,
+    marginTop: 15,
   },
 });
 
