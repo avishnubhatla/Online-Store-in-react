da81e1f2cb1d236a5e0179e53f8c8a830fb559d6
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-26 10:23:30 -0400

    Update Product.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/Product.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/Product.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Text, View, StyleSheet, Pressable } from 'react-native';
+import { Text, View, StyleSheet, Pressable, Image } from 'react-native';
 
-export function Product() {
+export function Product(props) {
   return (
-    <View>
+    <View style={styles.product}>
+      <Image source={{uri: props.src}} style={styles.image}/>
+      <View style={styles.textContainer}>
+        <Text style={styles.productNameText}>{props.productName}</Text>
+        <Text>{props.price}</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+  product: {
+    width: "100%",
+    height: 150,
+    flexDirection: "row",
+    margin: 10,
+  },
+  image: {
+    flex: 1,
+    objectFit: "fill"
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  productNameText: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
 });
 
 
