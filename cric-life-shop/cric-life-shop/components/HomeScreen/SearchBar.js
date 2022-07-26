14ff1e03bcaef97d00d75935071f60fd36c009d1
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-26 10:34:31 -0400

    Update SearchBar.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/SearchBar.js
@@ -1,11 +1,18 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { TextInput, Text, View, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export function SearchBar() {
+export function SearchBar(props) {
+  const [text, setText] = useState("")
+
+  const handleChange = text => {
+    setText(text)
+    props.onChangeText(text)
+  }
   return (
     <LinearGradient colors={['#FF0000', '#CC0033', "#990066", "#660099", "#3300CC", "#0000FF"]} style={styles.layout} start={{x: 0, y: 0.5}} end={{x: 1, y: 0.5}}>
-      <TextInput style={styles.searchBar} placeholder="Search CricLife Store"/>
+      <TextInput style={styles.searchBar} placeholder="Search CricLife Store" value={text}
+      onChangeText={text => handleChange(text)}/>
     </LinearGradient>
   )
 }
