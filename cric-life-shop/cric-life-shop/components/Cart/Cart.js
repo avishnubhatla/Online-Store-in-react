0cfae491aa6e8df1f41e4fe331acb78f17e3be84
AnikethL <102996935+AnikethL@users.noreply.github.com>
2022-07-29 17:35:21 -0400

    Update Cart.js

--- cric-life-shop/cric-life-shop/components/Cart/Cart.js
+++ cric-life-shop/cric-life-shop/components/Cart/Cart.js
@@ -1,5 +1,8 @@
 import React,{useState} from 'react';
 import { Text, View, StyleSheet, FlatList, ScrollView, Button,Image, TouchableOpacity, Pressable, SafeAreaView, TextInput, Alert } from 'react-native';
+import {CartMap} from './CartMap';
+import {SubtotalMap} from './SubtotalMap';
+import {styles} from './styles'
  
 export function Cart(props) {
   const [products, newProducts]=useState([
@@ -13,38 +16,7 @@ export function Cart(props) {
     {uri: 'https://m.media-amazon.com/images/I/916LckrjstS._AC_SX679_.jpg', name: "Cricket Pitch", price: "$300", numOf: 1, tPrice: "$300"},
     {uri: 'https://m.media-amazon.com/images/I/51GJDjJGHLL._AC_.jpg', name: "Cricket Kit", price: "$250", numOf: 1, tPrice: "$250"},
     {uri: 'https://m.media-amazon.com/images/I/51mI2UYeHGL._AC_SX466_.jpg', name: "Cricket Helmet", price: "$40", numOf: 1, tPrice: "$40"}])
-  
-  const removeItem = (index) => {
-    let stuff = [...products];
-    let stuffThing = stuff[index];
-    stuffThing.numOf=stuffThing.numOf-1;
-    stuffThing.tPrice = "$"+(stuffThing.price.slice(1)*stuffThing.numOf)
-    stuff[index]=stuffThing;
-    if(stuffThing.numOf===0){
-      newProducts([
-                ...products.slice(0, index),
-                ...products.slice(index + 1)
-      ]);
-    }
-    else{
-      newProducts(stuff);
-    }
-  }
-  const removeAll = (index) => {
-      newProducts([
-                ...products.slice(0, index),
-                ...products.slice(index + 1)
-      ]);
-  }
-  
-  const addItem = (index) => {
-    let stuff = [...products];
-    let stuffThing = stuff[index];
-    stuffThing.numOf=stuffThing.numOf+1;
-    stuffThing.tPrice = "$"+(stuffThing.price.slice(1)*stuffThing.numOf)
-    stuff[index]=stuffThing;
-    newProducts(stuff);
-  }
+
  
   const createTwoButtonAlert = () =>{
     Alert.alert(
@@ -60,72 +32,20 @@ export function Cart(props) {
       
     );
   }
-  let sum = 0;
   return (
     <View style = {styles.layout, styles.textContainer}>
       <ScrollView nestedScrollEnabled={true}>
-        <Text style={styles.cartText}>
-          Your Cart
-        </Text>
- 
-        {products.map((product) => {
-            return (
-              <View style={{flexDirection:"row"}}> 
-                <Image source={{uri: product.uri}} style={{width:150, height:140}}/>
-                <View >
-                  <Text style={styles.listTextBold}>{product.name} </Text>
-                  <Text style={styles.listText}>{product.tPrice}</Text>
-                  
-                  <View style={{flexDirection:"row"}}>
-                    <TouchableOpacity style={styles.buttonContainer} onPress={()=>addItem(products.indexOf(product))}>
-                      <Text style={styles.buttonText}>
-                        +
-                      </Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.buttonContainer} onPress={()=>removeItem(products.indexOf(product))}>
-                      <Text style={styles.buttonText}>
-                        -
-                      </Text>
-                    </TouchableOpacity> 
-                  </View>
- 
-                  <TouchableOpacity style={styles.buttonContainer} onPress={()=>removeAll(products.indexOf(product))}>
-                    <Text style={styles.buttonText}>
-                      Delete
-                    </Text>
-                  </TouchableOpacity>
- 
-                </View>
-                <TextInput value={product.numOf.toString()} style={{borderWidth:1, width:30, height:30}} editable={false}/>
-              </View>
-            );
-          })}
- 
+        
+        <CartMap productList = {products} setter = {newProducts}/>
+        
         <TouchableOpacity style={styles.buttonContainer} onPress={()=>createTwoButtonAlert()}>
           <Text style={styles.buttonText}>
             Clear Cart
           </Text>
         </TouchableOpacity> 
- 
-        <Text style={styles.medium}>
-          Subtotal:
-        </Text>
- 
-        {products.map((product) => {
-            sum+=parseInt(product.price.slice(1))
-            return (
-              <View style={{flexDirection:"row"}}> 
-                  <Text style={styles.listTextBold}>{product.name}, </Text>
-                  <Text style={styles.listText}>{product.numOf}, </Text>
-                  <Text style={styles.listText}>{product.tPrice}</Text>
-              </View>
-            );
-          })}
-        <Text style={styles.medium}>
-          Subtotal = ${sum}
-        </Text>
- 
+        
+        <SubtotalMap productList = {products} setter = {newProducts}/>    
+
         <TouchableOpacity style={styles.buttonContainer} onPress={props.onPress}>
           <Text style={styles.buttonText}>
             Buy
@@ -136,61 +56,6 @@ export function Cart(props) {
     </View>
   )
 }
- 
-const styles = StyleSheet.create({
-  layout:{
-    flex:5,
-  },
-  cartText: {
-    justifyContent: 'flex-start',
-    marginTop: 80,
-    fontWeight: 'bold',
-    fontSize: 50,
-    textAlign:'center',
-    color: "black"
-  },
-  medium:{
-    justifyContent: 'flex-start',
-    marginTop: 15,
-    fontSize: 30,
-    textAlign:'center',
-    color: "black",
-    fontWeight: "bold"
-  },
-  listTextBold:{
-    justifyContent: 'flex-start',
-    marginTop: 25,
-    alignSelf: "center",
-    fontSize: 20,
-    textAlign:'left',
-    color: "black",
-    fontWeight:"bold"
-  },
-  listText:{
-    justifyContent: 'flex-start',
-    marginTop: 25,
-    alignSelf: "center",
-    fontSize: 20,
-    textAlign:'left',
-    color: "black"
-  },
-  buttonContainer:{
-    flex:1,
-    justifyContent: 'flex-end',
-    marginBottom: 30,
-    horizontalMargin: 20,
-    height: 30,  
-    backgroundColor: '#092FB8',
-    alignItems: 'center',
-    button: {position: 'absolute', bottom:0},    
-  },
-  buttonText:{
-    justifyContent: 'center',
-    color: '#fff',
-    fontSize: 18,
-  }
- 
-});
  
  
 //<Image source={{uri: "https://m.media-amazon.com/images/I/51IwaYzGLCL._AC_SX679_.jpg"}}/>
