2f6e27846e596a585f1c0892037dcee58aaa4bea
AnikethL <102996935+AnikethL@users.noreply.github.com>
2022-08-28 14:23:31 -0400

    Update Cart.js

--- cric-life-shop/cric-life-shop/components/Cart/Cart.js
+++ cric-life-shop/cric-life-shop/components/Cart/Cart.js
@@ -8,7 +8,7 @@ export function Cart(props) {
   const [products, newProducts]=useState([
     { uri: "https://m.media-amazon.com/images/I/51IwaYzGLCL._AC_SX679_.jpg", name: "White Ball", price: "$7", numOf: 1, tPrice: "$7"},
     {uri:'https://images.squarespace-cdn.com/content/v1/5fd20db1750b345d8b151572/1607681222132-OIU32C99X3V195ZDG5AE/Red+Premier+Side.jpg?format=750w', name: "Red Ball", price: "$7", numOf: 1, tPrice: "$7"},
-    {uri: 'https://m.media-amazon.com/images/I/81bxuZCpdpL._AC_SY879_.jpg', name: 'Kookaburra Bat', price: "$100", numOf: 1, tPrice: "$7"},
+    {uri: 'https://m.media-amazon.com/images/I/81bxuZCpdpL._AC_SY879_.jpg', name: 'Kookaburra Bat', price: "$100", numOf: 1, tPrice: "$100"},
     {uri: 'https://bestcricketstore.com/wp-content/uploads/2022/01/e3e916c8-b859-46b3-b197-2c9c7d7c1f69.jpeg', name: "Cricket Stumps", price: "$70", numOf: 1, tPrice: "$70"},
     {uri: 'https://cdn.shopify.com/s/files/1/0020/7477/1565/products/first-pitch-bowler-pro-2-wheel-cricket-bowling-machine-16140464029805_1024x1024.jpg?v=1612371229', name: "Bowling Machine", price: "$600", numOf: 1, tPrice: "$600"},
     {uri: 'https://i.ebayimg.com/images/g/9SQAAOSwvGRh4CAH/s-l500.jpg', name: "Rope Ball", price: "$15", numOf: 1, tPrice: "$15"},
