04ce910629f5abc9ba2e12fc959cdbeb5b52d6c6
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-08-28 14:34:00 -0400

    Update App.js

--- cric-life-shop/cric-life-shop/App.js
+++ cric-life-shop/cric-life-shop/App.js
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from "./components/HomeScreen/HomeScreen";
 import { Cart } from "./components/Cart/Cart";
 import { Profile } from "./components/Profile/Profile";
-
+import {SignInScreen} from './components/Authentication/SigninScreen'
+import {SignUpScreen} from './components/Authentication/SignupScreen'
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <View style={styles.layout}>
+      <SignInScreen/>
+      <SignUpScreen/>
       <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name="Home" component={HomeScreen} />
