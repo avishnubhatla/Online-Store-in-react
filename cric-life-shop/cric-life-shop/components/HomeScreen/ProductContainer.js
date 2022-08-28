5044e5f155ef81092dd807d8da2771e7ed771daa
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-08-28 14:16:53 -0400

    Update ProductContainer.js

--- cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
+++ cric-life-shop/cric-life-shop/components/HomeScreen/ProductContainer.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react';
-import { TextInput, Text, View, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import * as React from 'react';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
+import { Product } from "./Product"
 
-export function SearchBar(props) {
-  const [text, setText] = useState("")
-
-  const handleChange = text => {
-    setText(text)
-    props.onChangeText(text)
-  }
+export function ProductContainer(props) {
   return (
-    <LinearGradient colors={['#FF0000', '#CC0033', "#990066", "#660099", "#3300CC", "#0000FF"]} style={styles.layout} start={{x: 0, y: 0.5}} end={{x: 1, y: 0.5}}>
-      <TextInput style={styles.searchBar} placeholder="Search CricLife Store" value={text}
-      onChangeText={text => handleChange(text)}/>
-    </LinearGradient>
+    <View style={styles.layout}>
+      <ScrollView>
+        <View style={styles.textContainer}>
+          <Text style={styles.welcomeText}>Start Your Cricketing Journey With CricLife!</Text>
+        </View>
+          {props.products.map(({uri, name, price }, index) => (
+          <Product src={uri} productName={name} price={price} onPress = {props.onPress} i={index}/>
+          ))}
+      </ScrollView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   layout: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flex: 5,
   },
-  searchBar: {
-    borderWidth: 2,
-    width: "80%",
-    height: "35%",
-    borderRadius: "10%",
-    backgroundColor: "white",
-    padding: 5,
-    marginTop: 15,
+  welcomeText: {
+    margin: 10,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  textContainer: {
+    alignItems: "center",
+
   },
 });
+
+
