f9fbbd4c693bd5e13b321a6e5cd7902cb23b6a2d
AnikethL <102996935+AnikethL@users.noreply.github.com>
2022-12-31 17:03:28 -0500

    Update firebase.js

--- cric-life-shop/cric-life-shop/components/firebase.js
+++ cric-life-shop/cric-life-shop/components/firebase.js
@@ -1,27 +1,21 @@
-import firebase from "firebase";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDh0OSUw5_pIp4BI7vbTXn5vogIfWImbqg",
-  authDomain: "chatauth-827a5.firebaseapp.com",
-  databaseURL: "https://chatauth-827a5-default-rtdb.firebaseio.com",
-  projectId: "chatauth-827a5",
-  storageBucket: "chatauth-827a5.appspot.com",
-  messagingSenderId: "637351100009",
-  appId: "1:637351100009:web:c81379e49a6f69bba7acf8"
+  apiKey: "AIzaSyDuw4vnGYXt9el8VEchaS_z3nxC81z3ZV4",
+  authDomain: "cric-b0640.firebaseapp.com",
+  projectId: "cric-b0640",
+  storageBucket: "cric-b0640.appspot.com",
+  messagingSenderId: "959795396143",
+  appId: "1:959795396143:web:9476998dfc446d79f650da",
+  measurementId: "G-86L20ZE9X5"
 };
 
 // Initialize Firebase
-let app;
-if (!firebase.apps.length) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app()
-}
-
-const auth = firebase.auth()
-
-export { auth };
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
