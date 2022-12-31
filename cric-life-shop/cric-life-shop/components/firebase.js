455864f6c2f17a54faa44db85e815663f5301b77
AnikethL <102996935+AnikethL@users.noreply.github.com>
2022-12-31 16:24:53 -0500

    Create firebase.js

---
+++ cric-life-shop/cric-life-shop/components/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh0OSUw5_pIp4BI7vbTXn5vogIfWImbqg",
+  authDomain: "chatauth-827a5.firebaseapp.com",
+  databaseURL: "https://chatauth-827a5-default-rtdb.firebaseio.com",
+  projectId: "chatauth-827a5",
+  storageBucket: "chatauth-827a5.appspot.com",
+  messagingSenderId: "637351100009",
+  appId: "1:637351100009:web:c81379e49a6f69bba7acf8"
+};
+
+// Initialize Firebase
+let app;
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
+
