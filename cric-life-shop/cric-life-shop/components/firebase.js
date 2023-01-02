1e3e8fc670727dbf2541c11302a0e89c5e377b4e
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2023-01-02 11:36:58 -0500

    Update firebase.js

--- cric-life-shop/cric-life-shop/components/firebase.js
+++ cric-life-shop/cric-life-shop/components/firebase.js
@@ -4,13 +4,13 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuw4vnGYXt9el8VEchaS_z3nxC81z3ZV4",
-  authDomain: "cric-b0640.firebaseapp.com",
-  projectId: "cric-b0640",
-  storageBucket: "cric-b0640.appspot.com",
-  messagingSenderId: "959795396143",
-  appId: "1:959795396143:web:9476998dfc446d79f650da",
-  measurementId: "G-86L20ZE9X5"
+  apiKey: "AIzaSyDh0OSUw5_pIp4BI7vbTXn5vogIfWImbqg",
+  authDomain: "chatauth-827a5.firebaseapp.com",
+  databaseURL: "https://chatauth-827a5-default-rtdb.firebaseio.com",
+  projectId: "chatauth-827a5",
+  storageBucket: "chatauth-827a5.appspot.com",
+  messagingSenderId: "637351100009",
+  appId: "1:637351100009:web:c81379e49a6f69bba7acf8"
 };
 
 // Initialize Firebase
